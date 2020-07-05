07a6b0251af48337f59fdd8f7bde59eff1a7e5b9
tangle5500 <345474054@qq.com>
2020-07-05 19:47:47 +0800

    init

---
+++ js/lock/lock.js
@@ -0,0 +1,21 @@
+checkCookie()
+
+// 获取 cookie
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name)==0) { return c.substring(name.length,c.length); }
+    }
+    return "";
+}
+
+// 检验 cookie
+function checkCookie() {
+    var user=getCookie("mto"); 
+    if (user != "mto100") {
+        // 跳转
+        window.location.href = "http://cmto.gq/login.html";
+    }
+}
